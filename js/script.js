cfe67309a793515dcebea5f3ebaf023a940199ec
Suraj Rathour <surajrathour0124@gmail.com>
2023-08-30 03:01:57 +0530

    Animation added with gsap library

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("This is testing print in JS");
+gsap.to('#navbar', {
+    backgroundColor: '#000',
+    duration: 0.5,
+    padding: '0.8rem 0px',
+    scrollTrigger: {
+        trigger: '#navbar',
+        scroller: 'body',
+        // markers: true,
+        start: 'top -10%',
+        end: 'top -11%',
+        scrub: 2
+    }
+});
+
+gsap.to('#main', {
+    backgroundColor: '#000',
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub:2
+    },
+})
